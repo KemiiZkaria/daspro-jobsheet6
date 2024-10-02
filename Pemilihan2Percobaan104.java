98474e5737f7d1545ac79ee1d4062221dfa479b9
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-10-02 18:40:43 +0700

    upload tugas jobsheet percobaan 1

---
+++ Pemilihan2Percobaan104.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan104 {
+    public static void main(String[] args) {
+        Scanner input04 = new Scanner(System.in);
+
+        System.out.print("Masukkan tahun yang anda inginkan: ");
+        int tahun = input04.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun " + tahun + " merupakan tahun kabisat");
+            }
+        } else
+            System.out.println("Tahun " + tahun + " bukan tahun kabisat");
+    }
+}
