dfab85d80f22f0c77c2517ca3c9e60695e6dd361
KemiiZkaria <ahmadkevinmalik01@gmail.com>
2024-10-03 07:54:55 +0700

    merubah equalsignorecase menjadi ignore

--- Pemilihan2Percobaan304.java
+++ Pemilihan2Percobaan304.java
@@ -13,7 +13,7 @@ public class Pemilihan2Percobaan304 {
         System.out.print("Masukkan besarnya penghasilan: ");
         penghasilan = input04.nextInt();
 
-        if (kategori.equalsIgnoreCase("pekerja")) {
+        if (kategori.equals("pekerja")) {
             if (penghasilan <= 2000000) {
                 pajak = 0.1;
             } else if (penghasilan <= 3000000) {
@@ -23,7 +23,7 @@ public class Pemilihan2Percobaan304 {
             }
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.print("Penghasilan bersih: " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
+        } else if (kategori.equals("pebisnis")) {
             if (penghasilan <= 2500000) {
                 pajak = 0.15;
             } else if (penghasilan <= 3500000) {
